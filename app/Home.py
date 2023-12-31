6256783f9854c67e121647e3faf161a98944c964
MuhammadSohaib <sohaib@sohaib-lpt.channelvas.prv>
2023-12-31 11:15:02 +0500

    fix path

--- app/Home.py
+++ app/Home.py
@@ -99,7 +99,7 @@ openai_api_key = st.sidebar.text_input('Enter your OpenAI API Key and hit Enter'
 openai.api_key = (openai_api_key)
 
 # load the file
-documents = SimpleDirectoryReader(input_files=["bio.txt"]).load_data()
+documents = SimpleDirectoryReader(input_files=["app/bio.txt"]).load_data()
 
 pronoun = info["Pronoun"]
 name = info["Name"]
