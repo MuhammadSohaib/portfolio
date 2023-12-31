6de7ea028d7899b49363f276a5c2578c956d9c68
MuhammadSohaib <sohaib@sohaib-lpt.channelvas.prv>
2023-12-31 11:13:35 +0500

    fix path

--- app/Home.py
+++ app/Home.py
@@ -25,7 +25,7 @@ def local_css(file_name):
     with open(file_name) as f:
         st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
         
-local_css("style/style.css")
+local_css("app/style/style.css")
 
 
 # loading assets
